53b08a07b2c39b9db6ba793af83e1c3b6aaa3efd
p1yux <soulpiyush09@gmail.com>
2025-02-15 11:44:32 +0530

    contacts

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -40,14 +40,14 @@ const Footer = () => {
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-white mb-3">Contact</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>K.T Marg Road, Vasai Road, Maharashtra, India</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 9372575530</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>learning.edu007@gmail.com</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Shahadara</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 999xx493xx</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>icodestarts@gmail.com</p>
                     <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" target='__blank' href="https://twitter.com/home?lang=en"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social" target='__blank' href="https://www.facebook.com/https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social" target='__blank' href="https://www.youtube.com/channel/UCWBZgfmIVxElVuTxsboMGNg"><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social" target='__blank' href="https://www.linkedin.com/feed/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social" target='__blank' href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" target='__blank' href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" target='__blank' href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" target='__blank' href=""><i class="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
